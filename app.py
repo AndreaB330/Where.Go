5d2aa93cc923ff84eb244506220164095099850c
Mykyta Holubakha <hilobakho@gmail.com>
2017-05-20 10:02:28 +0300

    Add API KEY

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ Kyiv = {'lat':50.45,'lng':30.52}
 
 @app.route('/')
 def homepage():
-    return render_template('index.html',API_KEY = API_KEY,center = Kyiv)
+    return render_template('index.html',API_KEY = app.config['API_KEY'],center = Kyiv)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
